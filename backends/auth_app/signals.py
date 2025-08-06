09585f58bc684864a0d28dd52da1f3bf4de43e77
Harihuynh2007 <hminhhai2000@gmail.com>
2025-08-06 14:40:39 +0700

    Centralize (tập trung) logic này vào một chỗ. Signal là nơi hoàn hảo cho việc này và xóa tất cả các dòng Workspace.objects.create(...) và Profile.objects.get_or_create(...) ra khỏi các views

--- backends/auth_app/signals.py
+++ backends/auth_app/signals.py
@@ -3,8 +3,11 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
 from .models import Profile
+from boards.models import Workspace
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_profile_for_new_user(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+        if not Workspace.objects.filter(owner=instance).exists():
+             Workspace.objects.create(name="Hard Spirit", owner=instance)
