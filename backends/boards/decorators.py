f12328ce91a4d508ad4d74ad1a87e6d121cecb79
Harihuynh2007 <hminhhai2000@gmail.com>
2025-08-09 02:24:28 +0700

    thay vì chỉ có require_board_editor, cần thêm decorator mới.

--- backends/boards/decorators.py
+++ backends/boards/decorators.py
@@ -1,7 +1,7 @@
 # boards/decorators.py
 from functools import wraps
 from rest_framework.exceptions import PermissionDenied
-from .permissions import check_board_edit_permission, check_board_view_permission, check_card_edit_permission
+from .permissions import check_board_edit_permission, check_board_view_permission, check_card_edit_permission,check_board_admin_permission
 
 def require_board_editor(board_getter):
     """
@@ -36,6 +36,17 @@ def require_card_editor(card_getter):
         def wrapper(self, request, *args, **kwargs):
             card = card_getter(self, request, *args, **kwargs)
             check_card_edit_permission(card, request.user)
+            return view_method(self, request, *args, **kwargs)
+        return wrapper
+    return decorator
+
+
+def require_board_admin(board_getter):
+    def decorator(view_method):
+        @wraps(view_method)
+        def wrapper(self, request, *args, **kwargs):
+            board = board_getter(self, request, *args, **kwargs)
+            check_board_admin_permission(board, request.user)
             return view_method(self, request, *args, **kwargs)
         return wrapper
     return decorator
